cfd4392c2c05c07208f73ca9b8e75f0ecc9851a3
wisut hantanong <wizzup@gmail.com>
2018-08-17 14:24:32 +0700

    add hie to nix-shell

--- shell.nix
+++ shell.nix
@@ -2,6 +2,6 @@
 let
   inherit (nixpkgs) pkgs;
   drv = import ./default.nix { inherit nixpkgs compiler; };
-  drvWithTools = pkgs.haskell.lib.addBuildDepends drv [ pkgs.cabal-install ];
+  drvWithTools = pkgs.haskell.lib.addBuildDepends drv [ pkgs.cabal-install pkgs.hie82 ];
 in
   if pkgs.lib.inNixShell then drvWithTools.env else drvWithTools
