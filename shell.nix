556e32fdfa804f6738060d9bf5123742cdbb3330
wisut hantanong <wizzup@gmail.com>
2018-09-06 12:18:01 +0700

    add hlint and brittany

--- shell.nix
+++ shell.nix
@@ -2,6 +2,10 @@
 let
   inherit (nixpkgs) pkgs;
   drv = import ./default.nix { inherit nixpkgs compiler; };
-  drvWithTools = pkgs.haskell.lib.addBuildDepends drv [ pkgs.cabal-install ];
+  drvWithTools = pkgs.haskell.lib.addBuildDepends drv (with pkgs;
+  [ cabal-install
+    hlint
+    haskellPackages.brittany
+  ]);
 in
   if pkgs.lib.inNixShell then drvWithTools.env else drvWithTools
